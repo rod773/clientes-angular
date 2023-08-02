8ec19eaaad1edde04410364cf9004574a0827374
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-02 16:08:59 +0200

    update material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+const material = [
+  MatSliderModule,
+  MatCheckboxModule,
+  FormsModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatButtonModule,
+  MatMenuModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
-  exports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, material],
+  exports: [material],
 })
 export class MaterialModule {}
